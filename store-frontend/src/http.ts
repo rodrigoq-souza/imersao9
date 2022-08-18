832fc39011c230bf5f79cc353f5135155fbebe7b
Rodrigo Queiroz <rodrigo.queiroz.souza@gmail.com>
2022-08-18 17:53:12 -0300

    Iniciando um repositório fork do projeto Imersão FullStack

--- store-frontend/src/http.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const http = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_STORE_API_URL
-})
-
-export default http;
--- store-frontend/src/pages/api/products/index.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { Product, products } from "../../../model";
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Product[]>
-) {
-  res.status(200).json(products);
-}
